bc2468fdbdaa60bc1a82e76153a6d1ea42abc5bb
coloradoalcocerja <80040045+coloradoalcocerja@users.noreply.github.com>
2021-03-04 12:30:58 +0100

    Update Practica.java
    Editando desde la web

--- Practica/src/practica/Practica.java
+++ Practica/src/practica/Practica.java
@@ -16,6 +16,9 @@ public class Practica {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        String nombre;
+        String apellido; 
+        //Editado desde la web
     }
     
 }
